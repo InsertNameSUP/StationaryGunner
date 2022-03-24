57794670568f4bd8b84d61b4649f34bc16463fe2
InsertNameSUP <Insert.NameSUP@gmail.com>
2022-03-24 19:39:58 +1100

    Background

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    public Color targetColor1;
+    public Color targetColor2;
+    public float speed;
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void Awake()
+    {
+        sr = GetComponent<SpriteRenderer>();   
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        sr.color = Color.Lerp(targetColor1, targetColor2, Mathf.PingPong(Time.time * speed, 1));
+    }
+}
